66db511f192eb79e6fa9caf8abebd554302a6d59
Ezabulbary <ezabul.bary@gmail.com>
2022-02-06 16:39:42 +0600

    added recap

--- recap.js
+++ recap.js
@@ -1,1 +1,34 @@
-var bookList = ["java", "javascript", "maths"]
+// variable
+var bookList = ['java', 'javascript', 'php', 'python'];
+
+// array 
+var javascriptIndex = bookList.indexOf('javascript');
+bookList[2] = 'html';
+console.log(bookList);
+
+bookList.push('english');
+bookList.pop();
+
+// condition 
+if(bookList == 'php'){
+    console.log('this on is my book listed');
+}
+else{
+    console.log('this is not my book listed');
+}
+
+// loop 
+// while loop 
+
+var i = 0;
+while(i < 20){
+    console.log(i);
+    console.log('ami muri khabo');
+    i++;
+}
+
+// for loop 
+
+for(i = o; i < 30; i++;){
+    console(i);
+}
