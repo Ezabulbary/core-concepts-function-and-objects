3df11d0a640a81a55d2b88097888832ce046458e
Ezabulbary <ezabul.bary@gmail.com>
2022-02-06 19:01:01 +0600

    function added

---
+++ function.js
@@ -0,0 +1,10 @@
+function startFan(){
+    console.log('walk to the path')
+    console.log('press the switch')
+}
+
+startFan();
+console.log('ami vat khabo');
+console.log('ami bazare zabo');
+console.log('ami ghurte jabo');
+startFan();
