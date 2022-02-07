8008803e8265d31d073c6403cac4a3db3fec503f
Ezabulbary <ezabul.bary@gmail.com>
2022-02-07 11:00:38 +0600

    object added

---
+++ object.js
@@ -0,0 +1,20 @@
+var phone = {
+    color: 'black',
+    price: 20000,
+    camara: 4,
+    memory: '8gb',
+}
+
+var book = {
+    title: 'javascript',
+    price: 490,
+    pages: 500,
+    author: 'junayd ahmed',
+}
+
+var student = {
+    id: 1524030,
+    name: 'Ezabul Bary',
+    department: 'folklore studies',
+    height: 5.6,
+}
